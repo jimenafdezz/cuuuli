b5ddf7945dc637693ae6a28c257a311f0c162423
jimenafdezz <jimena.fernandezlabrador@iesmiguelherrero.com>
2024-06-21 16:21:57 +0200

    sexo

---
+++ mavenproject7/src/main/java/com/mycompany/mavenproject7/Mavenproject7.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.mavenproject7;
+
+/**
+ *
+ * @author DAW107
+ */
+public class Mavenproject7 {
+
+    public static void main(String[] args) {
+        System.out.println("la uoltima y me voy");
+    }
+}
