b90013a4ae5735de400fbf1fc7ec99ef48c3cc8e
jimenafdezz <jimena.fernandezlabrador@iesmiguelherrero.com>
2024-06-21 16:14:10 +0200

    mecagoendcr

---
+++ cagoendios/src/main/java/com/mycompany/cagoendios/Cagoendios.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cagoendios;
+
+/**
+ *
+ * @author DAW107
+ */
+public class Cagoendios {
+
+    public static void main(String[] args) {
+        System.out.println("Hamenssdasas!");
+    }
+}
